7e93b9b4e2bcd24a246a943b72f0325633abba71
jaynro <jaynro@gmail.com>
2022-09-25 15:42:51 -0500

    Update vm.tf
    fixing network inteface on the VM

--- Project_Starter_Resources/terraform/modules/vm/vm.tf
+++ Project_Starter_Resources/terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
-resource "azurerm_network_interface" "" {
-  name                = "myVM"
+resource "azurerm_network_interface" "test" {
+  name                = "myVM-netwrok"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
@@ -11,12 +11,12 @@ resource "azurerm_network_interface" "" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "myVM"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1s"
-  admin_username      = ""
+  admin_username      = "azureuser"
   network_interface_ids = []
   admin_ssh_key {
     username   = "azureuser"
